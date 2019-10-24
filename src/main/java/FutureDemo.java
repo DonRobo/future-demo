cbc20812e5b230d5d8dfaf0946746870d1228bd4
RobertTieber <robert.tieber@prewave.ai>
2019-10-24 16:02:16 +0200

    Time measurement

--- src/main/java/FutureDemo.java
+++ src/main/java/FutureDemo.java
@@ -1,3 +1,6 @@
+import java.time.Duration;
+import java.time.Instant;
+
 import static java.lang.String.format;
 
 public class FutureDemo {
@@ -7,6 +10,7 @@ public class FutureDemo {
 	public static void main(String[] args) {
 		int[] numbers = setup(20);
 
+		Instant start = Instant.now();
 		int sum = 0;
 		for (int i = 0; i < numbers.length - 1; i += 2) {
 			sum += slowCalculator.add(numbers[i], numbers[i + 1]);
@@ -14,6 +18,7 @@ public class FutureDemo {
 
 		// should result in 210
 		System.out.println(format("Result: %d", sum));
+		System.out.println(format("Took %d seconds", Duration.between(start, Instant.now()).getSeconds()));
 	}
 
 	private static int[] setup(int countNumbers) {
