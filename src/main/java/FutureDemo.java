616c6b79a1f74af5e14bab9d8263a9264f172962
RobertTieber <robert.tieber@prewave.ai>
2019-10-24 16:03:22 +0200

    Multithreaded

--- src/main/java/FutureDemo.java
+++ src/main/java/FutureDemo.java
@@ -1,24 +1,41 @@
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static java.lang.String.format;
 
 public class FutureDemo {
 
 	private static SlowCalculator slowCalculator = new SlowCalculator();
+	private static ExecutorService executor = Executors.newCachedThreadPool();
 
 	public static void main(String[] args) {
 		int[] numbers = setup(20);
 
 		Instant start = Instant.now();
-		int sum = 0;
+		List<Future<Integer>> results = new ArrayList<>();
 		for (int i = 0; i < numbers.length - 1; i += 2) {
-			sum += slowCalculator.add(numbers[i], numbers[i + 1]);
+			final int j = i;
+			Future<Integer> result = executor.submit(() -> slowCalculator.add(numbers[j], numbers[j + 1]));
+			results.add(result);
 		}
+		int sum = results.stream().mapToInt(f -> {
+			try {
+				return f.get();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}).sum();
 
 		// should result in 210
 		System.out.println(format("Result: %d", sum));
 		System.out.println(format("Took %d seconds", Duration.between(start, Instant.now()).getSeconds()));
+
+		executor.shutdown();
 	}
 
 	private static int[] setup(int countNumbers) {
